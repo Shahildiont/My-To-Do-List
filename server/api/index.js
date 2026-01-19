aaf341e6027e1e6a557dc6a9ff48cf1b63d220c7
Shahil Ahamed <shahilahamed36@gmail.com>
2026-01-19 23:51:08 +0530

    change

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
-console.log("ENV:", process.env.MONGO_URI);
+// console.log("ENV:", process.env.MONGO_URI);
 
 const express = require('express');
 const mongoose = require('mongoose');
